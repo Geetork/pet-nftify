af8a637b98ca494db88730f2336b113f8b255e1f
geetork <galinaleespb@gmail.com>
2024-06-30 20:53:46 +0100

    deployment setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/pet-nftify/',
+    basePath: '/pet-nftify',
     images: {
       domains: ['geetork.github.io'],   
     },
