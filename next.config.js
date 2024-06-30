d74156545dcbc1fc541859b53960dac57de9d66d
geetork <galinaleespb@gmail.com>
2024-06-30 21:28:51 +0100

    deployment setup

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  distDir: "out",
   output: "export",
-  images: {
-    loader: "custom",
-    loaderFile: './imageLoader.js'
-  },
 }
   
   module.exports = nextConfig
